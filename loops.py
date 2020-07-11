2b96329e48676e9bb99667a62646af8246b29baa
Vaid26 <67861541+Vaid26@users.noreply.github.com>
2020-07-11 23:17:47 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,16 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+for nos in list1:
+    if nos>0:
+       print(nos,end=',')
+
+print()
+output=[]
+for num in list2:
+    if num>0:
+       output.append(num)
+print(output)
+
+
+
+
